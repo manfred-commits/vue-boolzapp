6b1241450858784bc20e2a58c4a4e8b2684466e8
manfred-commits <manfredipiraino@gmail.com>
2021-09-22 02:20:40 +0200

    added filter logic wip on contacts & chat

--- js/script.js
+++ js/script.js
@@ -89,7 +89,30 @@ var app = new Vue({
         messageInput:"",
         response:"",
         filter:"",
-        filObj:[],
+        filObj:[
+            {
+                name: 'Michele',
+                avatar: '_1',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
+            },
+        ]
     },
     mounted:function(){
         
@@ -138,7 +161,9 @@ var app = new Vue({
                 
                 let newObj= this.contacts.filter(el => el.name.includes(this.filter));
                 console.log(newObj);
-                this.filObj.push(newObj);
+                this.filObj=newObj;
+                console.log(this.filObj);
+
             }
         },
 
